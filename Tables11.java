dfde0a561d4cb6cb66032374a5d215ad4ae17b97
naveenkumar1710 <naveenthiru966@gmail.com>
2024-04-09 22:47:00 +0530

    changes in file

--- Tables11.java
+++ Tables11.java
@@ -2,13 +2,13 @@ package com.demo;
 
 public class Tables11 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			int i=1;
-			while(i<=150) {
-				System.out.println(i);
-				i=(i*11);
-			}
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int i = 1;
+        while (i <= 150) {
+            System.out.println(i);
+            i = (i * 11);
+        }
+    }
 
 }
